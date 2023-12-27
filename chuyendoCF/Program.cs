e13d3df1ba2ef7781f52d4c5d4ab077ad77b311d
Tho Kien <74567462+kin280@users.noreply.github.com>
2023-12-27 21:03:27 +0700

    Add project files.

---
+++ chuyendoCF/Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int choise = -1;
+
+        while (choise != 0)
+        {
+            Console.WriteLine("nhap lua chon");
+            choise = int.Parse(Console.ReadLine());
+            switch (choise)
+            {
+                case 0:
+                    {
+                        Environment.Exit(0);
+
+                    }
+                    break;
+                case 1:
+                    {
+                        Console.WriteLine(CF());
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine(FC());
+                    }
+                    break;
+                default:
+                    {
+                        Console.WriteLine("nhap lai");
+                    }
+                    break;
+            }
+        }
+    }
+
+    static int CF()
+    {
+        Console.WriteLine("nhap do C:");
+        int ch = int.Parse(Console.ReadLine());
+        int fa = 9 / 5 * ch + 32;
+        Console.WriteLine(fa);
+        return fa;
+    }
+
+    static int FC()
+    {
+        Console.WriteLine("nhap do F:");
+        int fa = int.Parse(Console.ReadLine());
+        int ch = 5 / 9 * (fa - 32);
+        Console.WriteLine(ch);
+        return ch;
+    }
+}
